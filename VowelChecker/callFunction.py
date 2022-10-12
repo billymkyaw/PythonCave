1b6cfad2d45d81a055a38360cf6db14ce432caf9
billymkyaw <96079553+billymkyaw@users.noreply.github.com>
2022-10-12 18:10:39 -0400

    Create callFunction.py

---
+++ VowelChecker/callFunction.py
@@ -0,0 +1,9 @@
+
+#importing vowelconso.py
+
+import vowelconso
+
+charList = vowelconso.checkVowel(['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+
+checkVowel(charList)
