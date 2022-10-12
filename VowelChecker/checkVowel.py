d8ec4d378e590d10ca79d0bf5eb590089c5b0a40
billymkyaw <96079553+billymkyaw@users.noreply.github.com>
2022-10-12 18:12:33 -0400

    Create checkVowel.py

---
+++ VowelChecker/checkVowel.py
@@ -0,0 +1,18 @@
+#Written by Billy M Kyaw
+
+#vowelconso.py that keep function for checkVowel
+def checkVowel(charList):
+    for i in charList:
+        if i == 'A':
+            print(i, "Vowel")
+        elif i == 'E':
+            print(i, "Vowel")
+        elif i == 'I':
+            print(i, "Vowel")
+        elif i == 'O':
+            print(i, "Vowel")
+        elif i == 'U':
+            print(i, "Vowel")
+        else:
+            print(i, "Consonant")
+                   
