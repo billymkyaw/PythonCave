5609edef562003b3c7cee2e9386cf80fabc39fd1
Billy M Kyaw <96079553+billymkyaw@users.noreply.github.com>
2022-10-19 13:14:48 -0400

    Create mainCallFunction.py

---
+++ ListFindFunction/mainCallFunction.py
@@ -0,0 +1,10 @@
+# Search the number
+
+from listFindFunction import listFind
+
+lst = [1, 18, 14, 15, 7]
+
+print(lst)
+
+print("Index @ 0: ", listFind(lst, 18))
+print("Index @ 1: ", listFind(lst, 7))
